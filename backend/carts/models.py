375fba6e93a358d2ad490eb6575d96a14187dc55
ArtemW2 <artem.sadykov10a@mail.ru>
2024-09-16 22:10:55 +0700

    add_carts

--- backend/carts/models.py
+++ backend/carts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits = 10, decimal_places = 2)
+    image = models.ImageField(upload_to = '', blank=True)
+    is_available = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        pass
