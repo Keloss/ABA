8fa7b7015330372190eab45bb7dc411650c09700
ArtemW2 <artem.sadykov10a@mail.ru>
2024-09-24 08:57:13 +0700

    superuser

--- backend/carts/models.py
+++ backend/carts/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from phonenumber_field.modelfields import PhoneNumberField
+#from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length = 50)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits = 10, decimal_places = 2)
-    image = models.ImageField(upload_to = '', blank=True)
-    is_available = models.BooleanField(default=True)
-    
+    name = models.CharField(max_length = 50, verbose_name = 'Название')
+    description = models.TextField(blank=True, verbose_name = 'Описание блюда')
+    price = models.DecimalField(max_digits = 10, decimal_places = 2, verbose_name = 'Цена(в рублях)')
+    image = models.ImageField(upload_to = '', blank=True, verbose_name = 'Картинка')
+    is_available = models.BooleanField(default=True, verbose_name = 'В наличии')
+
     class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
+        verbose_name = 'Блюдо'
+        verbose_name_plural = 'Блюда'
 
     def __str__(self):
-        pass
+        return self.name
+    
--- backend/carts/views.py
+++ backend/carts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+def home(request):
+
+    food_list = Product.objects.all()
+
+    context = {
+        'name': food_list.name,
+        'price': food_list.price,
+    }
+
+    return render(request, 'base.html', context = context)
